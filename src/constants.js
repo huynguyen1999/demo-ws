2716ff246c50cafb041571078f37200680f33b4e
IceH <conghuy.nguyentran.1999@gmail.com>
2024-12-31 13:24:20 +0700

    fix export/import file and add jenkinsfile for ci/cd

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,3 @@
-export const PORT = 8080;
+const PORT = 9999;
+
+module.exports = { PORT };
--- src/server.js
+++ src/server.js
@@ -1,21 +1,44 @@
-const { PORT } = require( "./constants" );
-const http = require( 'http' );
-const wsServer = require( "websocket" ).server;
+const { PORT } = require( "./constants.js" );
+const http = require( "http" );
+const WebSocketServer = require( "websocket" ).server;
 
-
+// Create an HTTP server
 const httpServer = http.createServer();
 
-httpServer.listen( PORT, () => `Server is listening on port ${ PORT }` );
-const gameWsServer = new wsServer( { httpServer } );
+// Start the HTTP server and log when it's listening
+httpServer.listen( PORT, () =>
+{
+    console.log( `Server is listening on port ${ PORT }` );
+} );
+
+// Create a WebSocket server and attach it to the HTTP server
+const gameWsServer = new WebSocketServer( { httpServer } );
 
 gameWsServer.on( "request", ( request ) =>
 {
     const connection = request.accept( null, request.origin );
-    connection.on( "open", () => console.log( "Connection opened" ) );
-    connection.on( "close", () => console.log( "Connection closed" ) );
+
+    console.log( "Client connected" );
+
+    // Event: Connection opened
+    connection.on( "open", () =>
+    {
+        console.log( "Connection opened" );
+    } );
+
+    // Event: Connection closed
+    connection.on( "close", () =>
+    {
+        console.log( "Connection closed" );
+    } );
+
+    // Event: Message received
     connection.on( "message", ( message ) =>
     {
-        console.log( `Received message from client: ${ message.utf8Data }` );
-        connection.send( `Echo: ${ message.utf8Data }` );
+        if ( message.type === "utf8" )
+        {
+            console.log( `Received message from client: ${ message.utf8Data }` );
+            connection.sendUTF( `Echo: ${ message.utf8Data }` );
+        }
     } );
 } );
